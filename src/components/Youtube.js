9c8bcfb1ff662f90d6dc70d845bbb3167f7ee6d2
siistimur <steven.indrayana.istimur@gmail.com>
2021-01-25 01:39:38 +0800

    add youtube.js

---
+++ src/components/Youtube.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+const Youtube = () => {
+  return (
+    <div>
+      <ReactPlayer url="https://youtu.be/y1HHzc_ml9Y" />
+    </div>
+  );
+};
+
+export default Youtube;
