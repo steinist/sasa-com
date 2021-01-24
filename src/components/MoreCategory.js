df20d496fee8f3679d6665949d5a003584c4fb4f
siistimur <steven.indrayana.istimur@gmail.com>
2021-01-25 01:41:13 +0800

    add more category

---
+++ src/components/MoreCategory.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const MoreCategory = () => {
+  return (
+    <div>
+      <h3 id="more-category-title" className="heading-title">
+        更多產品分類
+      </h3>
+      <div className="card-flex">
+        <img
+          src="https://cms.cdn.91app.hk/images/original/17/086148bb-2429-4ffd-bbec-6f73bfd281d8-1609410632-2r5wiif507_m.jpg"
+          alt=""
+        />
+        <img
+          src="https://cms.cdn.91app.hk/images/original/17/086148bb-2429-4ffd-bbec-6f73bfd281d8-1609410697-2b4novvvp7_m.jpg"
+          alt=""
+        />
+        <img
+          src="https://cms.cdn.91app.hk/images/original/17/086148bb-2429-4ffd-bbec-6f73bfd281d8-1609410715-4nmmnkirac_m.jpg"
+          alt=""
+        />
+        <img
+          src="https://cms.cdn.91app.hk/images/original/17/086148bb-2429-4ffd-bbec-6f73bfd281d8-1609410761-dv5j3bzykp_m.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MoreCategory;
