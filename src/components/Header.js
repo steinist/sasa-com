2e80ae8c1ab3c559adcfe596b23a5f5d083fc072
siistimur <steven.indrayana.istimur@gmail.com>
2021-01-27 16:31:41 +0800

    adding header dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUserCircle,
@@ -8,8 +8,19 @@ import {
   faChevronDown,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const showDropdown = (e) => {
+    console.log("hello");
+    setShow(!show);
+  };
+  const hideDropdown = (e) => {
+    setShow(false);
+  };
+
   return (
     <div id="header">
       <div className="top-header">
@@ -73,28 +84,155 @@ const Header = () => {
 
             <div className="nav-menu-main">
               <ul className="nav-menu-ul">
-                <li className="nav-menu-li">
-                  <h4 className="nav-menu-title nav-menu-word">Category</h4>
-                  <FontAwesomeIcon
-                    icon={faChevronDown}
-                    size="xs"
-                    color="rgb(68, 68, 68)"
-                  />
+                <li className="nav-menu-li nav-menu-category">
+                  <NavDropdown
+                    title="Category"
+                    className="nav-menu-title"
+                    id="basic-nav-dropdown"
+                    show={show}
+                    onMouseEnter={showDropdown}
+                    onMouseLeave={hideDropdown}
+                  >
+                    <div className="drop-submenu">
+                      <div className="drop-left">
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Skincare
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Makeup
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Fragrance
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Personal Care
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          For Men
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Baby & Mom
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Beauty & Slimming
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Health Care
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Prevention Care
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Household
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Snack Corner
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          EU & US brands
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Japanese / Korean brands
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-left"
+                          href="#"
+                        >
+                          Other brands
+                        </NavDropdown.Item>
+                      </div>
+                      <div className="drop-right">
+                        <NavDropdown.Item
+                          className="dropdown-item-right"
+                          href="#"
+                        >
+                          Hair Products
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-right"
+                          href="#"
+                        >
+                          Best Picks for Drying days
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-right"
+                          href="#"
+                        >
+                          Best Deals
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          className="dropdown-item-right"
+                          href="#"
+                        >
+                          New Arrival
+                        </NavDropdown.Item>
+                      </div>
+                    </div>
+                  </NavDropdown>
                 </li>
                 <li className="nav-menu-li">
-                  <h4 className="nav-menu-title">Best Deals</h4>
+                  <a href="#">
+                    <h4 className="nav-menu-title">Best Deals</h4>
+                  </a>
                 </li>
                 <li className="nav-menu-li">
-                  <h4 className="nav-menu-title">Productive Care</h4>
+                  <a href="#">
+                    <h4 className="nav-menu-title">Productive Care</h4>
+                  </a>
                 </li>
                 <li className="nav-menu-li">
-                  <h4 className="nav-menu-title">Hot Offers</h4>
+                  <a href="#">
+                    <h4 className="nav-menu-title">Hot Offers</h4>
+                  </a>
                 </li>
                 <li className="nav-menu-li">
-                  <h4 className="nav-menu-title">Sasa Outlet</h4>
+                  <a href="#">
+                    <h4 className="nav-menu-title">Sasa Outlet</h4>
+                  </a>
                 </li>
                 <li className="nav-menu-li">
-                  <h4 className="nav-menu-title">New Arrival</h4>
+                  <a href="#">
+                    <h4 className="nav-menu-title">New Arrival</h4>
+                  </a>
                 </li>
               </ul>
 
